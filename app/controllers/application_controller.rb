ca582909a1144de1b277e05391430b8f17f578ac
tatsuya.satake <tatsuya.satake.5x@stu.hosei.ac.jp>
2019-02-09 06:39:38 +0000

    post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,8 @@ class ApplicationController < ActionController::Base
       redirect_to login_url
     end
   end
+  
+  def counts(users)
+      @count_microposts = user.microposts.count
+  end
 end
