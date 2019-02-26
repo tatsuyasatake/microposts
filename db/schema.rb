ff6fdece9c5abfbdf082929ace5dccf0be94c63c
tatsuya.satake <tatsuya.satake.5x@stu.hosei.ac.jp>
2019-02-26 12:41:35 +0000

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190220092327) do
+ActiveRecord::Schema.define(version: 20190226122628) do
 
   create_table "favorites", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
